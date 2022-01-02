948f46bb589ab893a1a5328d10a5d090de3685f1
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-01-02 21:02:00 +0000

    handling http post /gcd

--- actix-gcd/src/main.rs
+++ actix-gcd/src/main.rs
@@ -1,9 +1,19 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+
+mod gcd;
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
 
 fn main() {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
     });
     println!("Serving on http://localhost:3000...");
     server
@@ -24,4 +34,19 @@ fn get_index() -> HttpResponse {
                 </form>
             "#,
             )
+}
+
+fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring.");
+    }
+    let response = 
+        format!("The greatest common divisor of the numbers {} and {} \
+            is <b>{}</b>\n",
+            form.n, form.m, gcd::gcd(form.n, form.m));
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
 }
