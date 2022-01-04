1a1c3ce67dc470bd81a84e8253d947bd924bc408
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-01-04 23:14:32 +0000

    cmd args parsed

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -6,8 +6,30 @@ use image::png::PNGEncoder;
 use std::fs::File;
 use std::str::FromStr;
 
+use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1.0,20", args[0]);
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x')
+        .expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3])
+        .expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4])
+        .expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds)
+        .expect("error writing PNG file");
 
 }
 
@@ -62,7 +84,7 @@ fn render(pixels: &mut [u8],
         upper_left: Complex<f64>,
         lower_right: Complex<f64>
     ) {
-    assert!(pixels.len() == bounds.0 * * bounds.1);
+    assert!(pixels.len() == bounds.0 * bounds.1);
     for row in 0..bounds.1 {
         for column in 0..bounds.0 {
             let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
@@ -75,12 +97,12 @@ fn render(pixels: &mut [u8],
     }
 }
 
-fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) 
-    -> Result<(), std::io::Error> {
-    let output = File::create(filename)?;
-    let encoder = PNGEncoder::new(output);
-    encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
-    Ok(());
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> 
+    Result<(), std::io::Error> {
+        let output = File::create(filename)?;
+        let encoder = PNGEncoder::new(output);
+        encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+        Ok(())
 }
 
 #[test]
