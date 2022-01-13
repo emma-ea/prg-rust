5ccadac684b3c0587aac92e282f5d6c6c4c875d8
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-01-13 18:13:54 +0000

    init window error handled

--- tic-tac-toe/src/main.rs
+++ tic-tac-toe/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
 
     events_loop.run(move | ev, wtarget, control_flow | {
         let hmonitor = wtarget.primary_monitor();
-        set_window_attrib(&display);
+        set_window_attrib(display).expect("SET_WINDOW_ATTRIB_ERROR");
 
         let fps = time::Instant::now() + time::Duration::from_millis(500);
         *control_flow = event_loop::ControlFlow::WaitUntil(fps);
@@ -34,11 +34,11 @@ fn init_window<T>(events_loop: &event_loop::EventLoop<T>) -> glium::Display {
         .with_inner_size(dpi::LogicalSize::new(500.0, 500.0))
         .with_title("tic-tac-toe");
     let ctx = glutin::ContextBuilder::new();
-    glium::Display::new(wb, ctx, *events_loop).unwrap()
+    glium::Display::new(wb, ctx, events_loop).unwrap()
 }
 
-fn set_window_attrib<E>(display: &glium::Display) -> Result<glium::Frame, E> {
-    let mut frame = *display.draw();
+fn set_window_attrib<E>(display: glium::Display) -> Result<glium::Frame, E> {
+    let mut frame = display.draw();
     frame.clear_color(0.0, 0.5, 0.0, 1.0);
-    frame.finish()
+    Ok(frame.finish())
 }
