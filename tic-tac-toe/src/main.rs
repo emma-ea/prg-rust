95fb963294cd7256c5a5e8b652e0fef8f5fea3d6
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-01-13 20:45:57 +0000

    path statement with no effect fixed

--- tic-tac-toe/src/main.rs
+++ tic-tac-toe/src/main.rs
@@ -14,13 +14,13 @@ fn main() {
         let hmonitor = wtarget.primary_monitor();
         set_window_attrib(&display).expect("SET_WINDOW_ATTRIB_ERROR");
 
-        let fps = time::Instant::now() + time::Duration::from_millis(500);
+        let fps = time::Instant::now() + time::Duration::from_nanos(16_666_667);
         *control_flow = event_loop::ControlFlow::WaitUntil(fps);
 
         match ev {
             event::Event::WindowEvent {event, ..} => match event {
                 event::WindowEvent::CloseRequested => {
-                    event_loop::ControlFlow::Exit;
+                    *control_flow = event_loop::ControlFlow::Exit;
                     return;
                 },
                 _ => return,
@@ -32,7 +32,7 @@ fn main() {
 
 fn init_window<T>(events_loop: &event_loop::EventLoop<T>) -> glium::Display {
     let wb = window::WindowBuilder::new()
-        .with_inner_size(dpi::LogicalSize::new(500.0, 500.0))
+        .with_inner_size(dpi::LogicalSize::new(400.0, 400.0))
         .with_title("tic-tac-toe");
     let ctx = glutin::ContextBuilder::new();
     glium::Display::new(wb, ctx, events_loop).unwrap()
@@ -40,6 +40,6 @@ fn init_window<T>(events_loop: &event_loop::EventLoop<T>) -> glium::Display {
 
 fn set_window_attrib(display: &glium::Display) -> Result<(), SwapBuffersError> {
     let mut frame = display.draw();
-    frame.clear_color(0.0, 0.5, 0.0, 1.0);
+    frame.clear_color(0.0, 0.0, 0.1, 1.0);
     frame.finish()
 }
